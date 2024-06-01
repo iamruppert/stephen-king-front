cb03f1825d9ba124f701acf8b44c749db5dc25f4
iamruppert <lukaszziolkiewicz@tutanota.com>
2024-06-01 02:43:50 +0200

    Add menu animation on project page

--- js/about.js
+++ js/about.js
@@ -1,11 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("section");
 
-    sections.forEach((section, index) => {
-        if (index % 2 === 0) {
-            section.classList.add("animate-slide-right");
-        } else {
-            section.classList.add("animate-slide-left");
-        }
-    });
+  const sections = document.querySelectorAll("section");
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  const menuOverlay = document.getElementById('menuOverlay');
+  const closeMenu = document.getElementById('closeMenu');
+  const menuContent = document.querySelector('#menuOverlay');
+
+  sections.forEach((section, index) => {
+    if (index % 2 === 0) {
+      section.classList.add("animate-slide-right");
+    } else {
+      section.classList.add("animate-slide-left");
+    }
+  });
+
+  navbarToggler.addEventListener('click', function () {
+    menuOverlay.classList.add('show');
+    menuOverlay.classList.remove('hide');
+  });
+
+  closeMenu.addEventListener('click', function () {
+    menuOverlay.classList.add('hide');
+    setTimeout(() => {
+      menuOverlay.classList.remove('show');
+    }, 500);
+  });
+
+  menuContent.addEventListener('click', function (event) {
+    event.stopPropagation();
+  });
+
+  menuOverlay.addEventListener('click', function (event) {
+    if (!menuContent.contains(event.target)) {
+      menuOverlay.classList.add('hide');
+      setTimeout(() => {
+        menuOverlay.classList.remove('show');
+      }, 500);
+    }
+  });
+
 });
